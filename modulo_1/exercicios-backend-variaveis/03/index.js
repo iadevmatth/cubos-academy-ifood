b5fdd43b1d70319a4737cb78595e78be304fa65e
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-19 20:06:26 -0300

    shoes discount

---
+++ modulo_1/exercicios-backend-variaveis/03/index.js
@@ -0,0 +1,9 @@
+let money = 80;
+let valueShoes = 129.99;
+let discount = null;
+
+discount = money * 100;
+discount = discount / valueShoes
+discount = 100 - discount
+
+console.log(`You need of ${discount} percent of discount!`)
