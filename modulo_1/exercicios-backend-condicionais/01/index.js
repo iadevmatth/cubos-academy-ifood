d81bba0ccb2718400cb29b7ba1f58cd466005052
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-20 19:14:58 -0300

    Rock, Paper, Scissors

--- modulo_1/exercicios-backend-condicionais/01/index.js
+++ modulo_1/exercicios-backend-condicionais/01/index.js
@@ -1,4 +1,14 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada1 = "papel"
+const jogada2 = "papel"
 
-//seu código aqui
+if (jogada1 === jogada2) {
+  console.log("Pie");
+} else if (jogada1 === "pedra" && jogada2 !== "papel") {
+  console.log(`jogada1 ${jogada1} winner.`)
+} else if (jogada1 === "papel" && jogada2 !== "tesoura") {
+  console.log(`jogada1 ${jogada1} winner.`)
+} else if (jogada1 === "tesoura" && jogada2 !== "pedra") {
+  console.log(`jogada1 ${jogada1} winner.`)
+} else {
+  console.log(`jogada2 ${jogada2} winner.`)
+}
