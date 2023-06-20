f6be72e4b4c386cb9160e318f37dbaadd6ab82b8
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-20 02:37:01 -0300

    Total area of the cylinder

---
+++ modulo_1/exercicios-backend-variaveis/13/index.js
@@ -0,0 +1,6 @@
+let baseRadius = 3;
+let heightCylinder = 1;
+
+const cylinder = 2 * baseRadius * (baseRadius + heightCylinder)
+
+console.log(`For cylinder with area ${baseRadius} and heigth ${heightCylinder}, your total area is: ${cylinder}`)
