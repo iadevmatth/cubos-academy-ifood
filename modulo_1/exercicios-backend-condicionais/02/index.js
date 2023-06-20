50da195b7794d0e28f14b18cb9d6c53a2a4323e2
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-20 19:58:11 -0300

    Correction words

--- modulo_1/exercicios-backend-condicionais/02/index.js
+++ modulo_1/exercicios-backend-condicionais/02/index.js
@@ -2,7 +2,7 @@ const jogada1 = 5;
 const jogada2 = 4;
 
 if ((jogada1 + jogada2) % 2 === 0) {
-  console.log(`Par winner.`)
+  console.log(`Even winner.`)
 } else {
-  console.log(`Impar winner.`)
+  console.log(`Odd winner.`)
 }
