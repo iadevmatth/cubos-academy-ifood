7db56a54954e50ee7331db74116489b6449718e2
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-20 22:16:46 -0300

    Defining Positions

--- modulo_1/exercicios-backend-condicionais/06/index.js
+++ modulo_1/exercicios-backend-condicionais/06/index.js
@@ -1,3 +1,22 @@
 const alturaEmCm = 185;
 
-//seu código aqui
+if (alturaEmCm >= 180) {
+  if (alturaEmCm <= 185) {
+    console.log("APROVADO")
+    console.log("LÍBERO")
+  } else if (alturaEmCm <= 195) {
+    console.log("PONTEIRO")
+  } else if (alturaEmCm <= 205) {
+    console.log("OPOSTO")
+  } else if (alturaEmCm > 205) {
+    console.log("CENTRAL")
+  }
+} else {
+  console.log("REPROVADO")
+}
+
+// Caso o jogador tenha menos de 180cm, o programa deve imprimir REPROVADO
+// Caso tenha entre 180cm e 185cm, o programa deve imprimir LÍBERO
+// Caso tenha entre 186cm e 195cm, o programa deve imprimir PONTEIRO
+// Caso tenha entre 196cm e 205cm, o programa deve imprimir OPOSTO
+// Caso tenha mais de 205cm, o programa deve imprimir CENTRAL
