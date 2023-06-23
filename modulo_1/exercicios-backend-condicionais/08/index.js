f6c573fd39bf41a000562145da8fe694dbc1f048
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-22 21:39:56 -0300

    Rollercoaster

--- modulo_1/exercicios-backend-condicionais/08/index.js
+++ modulo_1/exercicios-backend-condicionais/08/index.js
@@ -3,28 +3,12 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
-
-// NÃO SÃO PERMITIDAS:
-
-// Menores de 12 anos
-// Maiores de 65 anos
-// Que possuam qualquer patologia cardíaca
-// Menores de 150cm de altura
-
-// PERMITIDAS
-// 10 reais caso a pessoa seja estudante ou menor de 18 anos (meia entrada)
-// 20 reias, caso contrário
-
-//IMPRIMIR
-// ACESSO NEGADO caso a pessoa não possa brincar
-// 10 reais caso esse seja o valor que a pessoa deve pagar para brincar
-// 20 reais caso esse seja o valor que a pessoa deve pagar para brincar
-
-if (idade < 12) {
-  console.log("")
-  if (idade > 65) {
-    console.log("ACESSO NEGADO: idade superior a permitida.")
-  } else if ("") {
-    console.log("R$20,00")
-  }
+if (idade < 12 || idade > 65 || altura < 150) {
+  console.log("Acesso Negado")
+} else if (possuiPatologia === true) {
+  console.log("Acesso Negado")
+} else if (idade <= 18 && ehEstudante === true) {
+  console.log("10 reais")
+} else {
+  console.log("20 reais")
 }
