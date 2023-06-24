fa4dde2b056b82e19a5fd1adf6f04a4cb08975d9
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-24 17:54:57 -0300

    resolution exercise

--- modulo_1/exercicios-backend-condicionais/09/index.js
+++ modulo_1/exercicios-backend-condicionais/09/index.js
@@ -1,1 +1,9 @@
-const nota = 8.5;
+let nota = 8.5;
+
+const novaNota = (nota >= 9 && nota <= 10) ? "A" :
+  (nota >= 8 && nota < 9) ? "B" :
+    (nota >= 6 && nota < 8) ? "C" :
+      (nota >= 4 && nota < 6) ? "D" :
+        (nota < 4) ? "E" : "You weren't supposed to see that."
+
+console.log(`Sua nota foi: ${novaNota}`)
