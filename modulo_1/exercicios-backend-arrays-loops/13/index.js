32595852864fcea4ee7f05d2ce75c03f3aa76fb9
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-25 18:50:35 -0300

    Resolution exercise 13

--- modulo_1/exercicios-backend-arrays-loops/13/index.js
+++ modulo_1/exercicios-backend-arrays-loops/13/index.js
@@ -1,2 +1,10 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+let ligados = 0;
+
+for (let disjuntor of disjuntores) {
+  if (disjuntor) {
+    console.log(ligados)
+  }
+  ligados++
+}
