2eeb622e56f03e66d5f46c35ef47ce64ce33f909
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-25 01:46:46 -0300

    resolution exercise 02

--- modulo_1/exercicios-backend-arrays-loops/02/index.js
+++ modulo_1/exercicios-backend-arrays-loops/02/index.js
@@ -1,1 +1,14 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+const letras = ["A", "a", "B", "C"];
+
+let letrasEncontradas = [];
+
+for (let letra of letras) {
+  if (letra === "E" || letra === "e") {
+    letrasEncontradas.push(letra)
+  }
+}
+
+const nenhumaEncontrada = (letrasEncontradas.length === 0) ? "Nenhuma letra 'E' ou 'e' foram encontradas" :
+  `foram encontradas ${letrasEncontradas.length} letras "E" ou "e"`
+
+console.log(nenhumaEncontrada)
