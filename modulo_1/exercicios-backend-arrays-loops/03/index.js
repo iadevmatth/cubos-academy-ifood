338d82a5389cbbbf62c34ef051f2d7e686da8ab3
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-25 02:12:41 -0300

    Resolution exercise 03

--- modulo_1/exercicios-backend-arrays-loops/03/index.js
+++ modulo_1/exercicios-backend-arrays-loops/03/index.js
@@ -1,2 +1,10 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+const numeros = [54, 22, 14, 87, 284];
 
+for (num of numeros) {
+  if (num === 10) {
+    console.log(numeros.indexOf(num))
+  } else {
+    console.log("-1")
+    break
+  }
+}
