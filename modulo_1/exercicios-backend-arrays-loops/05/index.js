ed2ea1bfdd9f84b2a432c3611b6a761ff2019346
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-25 02:46:42 -0300

    Resolution exercise 05

--- modulo_1/exercicios-backend-arrays-loops/05/index.js
+++ modulo_1/exercicios-backend-arrays-loops/05/index.js
@@ -1,1 +1,11 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+
+const arrayPar = [];
+
+for (let par of original) {
+  if (par % 2 === 0) {
+    arrayPar.push(par)
+  }
+}
+
+console.log(arrayPar)
