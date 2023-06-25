81d9a092d9690e4677fa9812aad06670cbebb219
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-24 22:40:15 -0300

    added exercises arrays and loops

--- modulo_1/exercicios-backend-condicionais/10/index.js
+++ modulo_1/exercicios-backend-condicionais/10/index.js
@@ -1,1 +1,12 @@
-const caractere = "E";
+let caractere = "1";
+
+if (caractere.toLowerCase === true) {
+  console.log(`${caractere}: minúscula.`)
+  if (caractere === !isNaN(parseFloat(String))) {
+    console.log(`${caractere}: Não é um número`)
+  } else {
+    console.log("")
+  }
+} else {
+  console.log(`${caractere}: maiúscula`)
+}
