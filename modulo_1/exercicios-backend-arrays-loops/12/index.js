7e77ed4e3736b982088929a2ec5be29bf521301a
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-25 16:50:04 -0300

    Resolution exercise 12

--- modulo_1/exercicios-backend-arrays-loops/12/index.js
+++ modulo_1/exercicios-backend-arrays-loops/12/index.js
@@ -1,3 +1,10 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+while (filaDeDentro.length < 5) {
+  const firstPeople = filaDeFora.shift(); // Variavel criada para receber o primeiro valor da array.
+  filaDeDentro.push(firstPeople)
+}
+
+console.log(filaDeDentro)
+console.log(filaDeFora)
