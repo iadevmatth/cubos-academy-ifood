4f4d7c1479b2b526b073f8e6072e8a2d1a4cb94b
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-25 05:45:30 -0300

    Resolution exercise 11

--- modulo_1/exercicios-backend-arrays-loops/11/index.js
+++ modulo_1/exercicios-backend-arrays-loops/11/index.js
@@ -1,2 +1,10 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
 
+let fake = [];
+
+for (let number of original) {
+  if ((number > 100) || (number > 10 && number < 20)) {
+    fake.push(number)
+  }
+}
+console.log(fake)
