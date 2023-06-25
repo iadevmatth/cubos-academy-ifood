208592552dfb4936443dc4141f25cb1520dc1d19
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-25 03:40:17 -0300

    Resolution exercise 06

--- modulo_1/exercicios-backend-arrays-loops/06/index.js
+++ modulo_1/exercicios-backend-arrays-loops/06/index.js
@@ -1,2 +1,17 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
 
+let sumNumbers = [];
+
+for (let sum of numeros) {
+  if (sum % 2 === 0) {
+    sumNumbers.push(sum)
+  }
+}
+
+let sumTotal = 0;
+
+for (n of sumNumbers) {
+  sumTotal += n
+}
+
+console.log(sumTotal)
