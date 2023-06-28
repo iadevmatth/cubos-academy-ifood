f318aae83465a37851315d4dddc108b77dd78814
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-28 20:41:59 -0300

    Exercicio resolvido, com ajuda do monitor Paulo Cesar [FullStack]

---
+++ modulo_1/exercicios-backend-objetos/06/index.js
@@ -0,0 +1,37 @@
+const usuarios = [
+  {
+    nome: "João",
+    idade: 25,
+  },
+  {
+    nome: "Ana",
+    idade: 18,
+  },
+  {
+    nome: "Beatriz",
+    idade: 15,
+  },
+  {
+    nome: "Carlos",
+    idade: 16,
+  },
+  {
+    nome: "Antonio",
+    idade: 32,
+  },
+]
+
+const jovens = [];
+
+const adultos = [];
+
+for (let usuario of usuarios) {
+  if (usuario.idade > 17) {
+    adultos.push(usuario)
+  } else {
+    jovens.push(usuario)
+  }
+};
+
+console.log(jovens)
+console.log(adultos)
