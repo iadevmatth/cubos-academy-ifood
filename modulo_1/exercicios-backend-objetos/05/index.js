e2c812ebc6c16d5a68cc047cb76a0b73274e1c79
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-06-28 20:36:39 -0300

    Adicionado uma outra forma de resolver

--- modulo_1/exercicios-backend-objetos/05/index.js
+++ modulo_1/exercicios-backend-objetos/05/index.js
@@ -23,8 +23,15 @@ const newClasses = [
   }
 ];
 
+
+// Mais liberdade de manipulação
 for (let aula = 0; aula < newClasses.length; aula++) {
   cursos.aulas.push(newClasses[aula])
 }
+
+// Situação mais robusta
+// for (let item of newClasses) {
+//   cursos.aulas.push(item)
+// }
 
 console.log(cursos)
