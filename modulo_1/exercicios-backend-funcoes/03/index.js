827845ab198f9d7462b373b849c126455444d67b
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-07-04 23:20:59 -0300

    part 02 of code

--- modulo_1/exercicios-backend-funcoes/03/index.js
+++ modulo_1/exercicios-backend-funcoes/03/index.js
@@ -1,4 +1,5 @@
 const carrinho = {
+    // Formato padrão do carrinho dentro do objeto.
     nomeDoCliente: "Guido Bernal",
     produtos: [
         {
@@ -14,7 +15,7 @@ const carrinho = {
             precoUnit: 5000
         }
     ],
-
+    // Imprime de forma resumida a quantidade de produto e totala pagar.
     imprimirResumo: function () {
         let totalQtdProdutos = 0;
         let valorTotal = 0;
@@ -26,6 +27,22 @@ const carrinho = {
         console.log(`Total de itens: ${totalQtdProdutos}`);
         console.log(`Total a pagar: R$ ${(valorTotal / 100).toFixed(2)}`);
     },
+    // Adiciona um novo produto ao carrinho levando em consideracao se ele já existe pelo ID.
+    // Se sim, ele adiciona apenas a quantidade. Se não, adiciona todo o produto.
+    addProduto: function (carrinho, produto) {
+        let itemExistente = false
+        for (let item of carrinho.produtos) {
+            if (produto.id === item.id) {
+                item.qtd += produto.qtd
+                itemExistente = true
+                break
+            }
+        };
+
+        if (!itemExistente) {
+            carrinho.produtos.push(produto)
+        }
+    }
 };
 
 const novaBermuda = {
@@ -35,11 +52,15 @@ const novaBermuda = {
     precoUnit: 5000
 }
 
-// function addProdutoAoCarrinho(carrinho, produto) {
-//     for (let item of carrinho.produtos) {
-//         if (produto === item.produtos)
-//     }
-// };
+const novoTenis = {
+    id: 3,
+    nome: "Tenis",
+    qtd: 1,
+    precoUnit: 10000
+}
 
-// addProdutoAoCarrinho(carrinho, novaBermuda)
+
+carrinho.addProduto(carrinho, novaBermuda)
+carrinho.addProduto(carrinho, novoTenis)
 carrinho.imprimirResumo();
+console.log(carrinho.produtos)
