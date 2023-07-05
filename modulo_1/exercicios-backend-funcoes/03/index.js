c94b73cdf470a0e101652af7f94cf654433c4d8b
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-07-04 21:00:32 -0300

    part 01 of code

--- modulo_1/exercicios-backend-funcoes/03/index.js
+++ modulo_1/exercicios-backend-funcoes/03/index.js
@@ -13,5 +13,33 @@ const carrinho = {
             qtd: 2,
             precoUnit: 5000
         }
-    ]
+    ],
+
+    imprimirResumo: function () {
+        let totalQtdProdutos = 0;
+        let valorTotal = 0;
+        for (let item of carrinho.produtos) {
+            totalQtdProdutos += item.qtd
+            valorTotal += item.qtd * item.precoUnit
+        };
+        console.log(`Cliente: ${carrinho.nomeDoCliente}`);
+        console.log(`Total de itens: ${totalQtdProdutos}`);
+        console.log(`Total a pagar: R$ ${(valorTotal / 100).toFixed(2)}`);
+    },
+};
+
+const novaBermuda = {
+    id: 2,
+    nome: "Bermuda",
+    qtd: 3,
+    precoUnit: 5000
 }
+
+// function addProdutoAoCarrinho(carrinho, produto) {
+//     for (let item of carrinho.produtos) {
+//         if (produto === item.produtos)
+//     }
+// };
+
+// addProdutoAoCarrinho(carrinho, novaBermuda)
+carrinho.imprimirResumo();
