c31c7b05fc6f6d3a4d09372c04d37592e42468d3
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-07-05 20:29:24 -0300

    Resolution exercise 01 method

--- modulo_1/exercicios-backend-metodos-strings/01/index.js
+++ modulo_1/exercicios-backend-metodos-strings/01/index.js
@@ -1,1 +1,18 @@
 const comentario = "Esse COVID é muito perigoso!";
+
+function blockedComment(comentario) {
+  // transforma toda a frase em maiúsculo para facilitar a verificação.
+  const commentModified = comentario.toUpperCase();
+  const ifBlocked = "Comentário bloqueado por conter palavras proibidas"
+  // Caso a verificação encontre as palavras, transforma o comentário no retorno padrão.
+  if (commentModified.includes("COVID") || commentModified.includes("PANDEMIA")) {
+    comentario = ifBlocked
+    console.log(comentario)
+  } else {
+    // Caso não encontre, mostra o comentário originalmente escrito.
+    console.log("Comentário autorizado.");
+    console.log(comentario)
+  }
+};
+
+blockedComment(comentario);
