6f6166971c6234899a1b88f7ba4810fcb372569e
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-07-08 03:11:37 -0300

    Friendly URL

--- modulo_1/exercicios-backend-metodos-strings/03/index.js
+++ modulo_1/exercicios-backend-metodos-strings/03/index.js
@@ -1,1 +1,15 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+
+function urlFormatada(texto) {
+  if (texto) {
+    let textoFormatado = texto.toLowerCase();
+    while (textoFormatado.includes(" ")) {
+      textoFormatado = textoFormatado.replace(" ", "-")
+    };
+    console.log(textoFormatado)
+  } else if (!texto) {
+    console.log("Url Invalida.")
+  }
+};
+
+urlFormatada(texto)
