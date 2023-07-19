82cca39d9280e16ec55a87bbff6f35bccd1bd102
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-07-19 12:40:29 -0300

    Array methods three contents

---
+++ modulo_1/distanciaentredoispontos.js
@@ -0,0 +1,34 @@
+function processData(input) {
+  // Separa o input em linhas
+  const linhas = input.split("\n");
+
+  // Obtém o número de funcionários
+  const numFuncionarios = parseInt(linhas[0]);
+
+  // Inicializa a maior distância como 0
+  let maiorDistancia = 0;
+
+  // Percorre as linhas com as coordenadas dos funcionários
+  for (let i = 1; i <= numFuncionarios; i++) {
+
+    // Obtém as coordenadas X e Y do funcionário atual
+    const [x1, y1] = linhas[i].split(" ").map(Number);
+
+    // Compara a distância do funcionário atual com todos os outros funcionários
+    for (let j = i + 1; j <= numFuncionarios; j++) {
+      // Obtém as coordenadas X e Y do outro funcionário
+      const [x2, y2] = linhas[j].split(" ").map(Number);
+
+      // Calcula a distância entre os dois pontos
+      const distancia = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+      // Atualiza a maior distância, se necessário
+      if (distancia > maiorDistancia) {
+        maiorDistancia = distancia;
+      }
+    }
+  }
+
+  // Imprime a maior distância encontrada
+  console.log(maiorDistancia.toFixed(1));
+};
