543d314be9e9af39134f9bb87bc39e237a7aab6a
Matheus Oliveira <matheus.devbackend@gmail.com>
2023-07-25 23:26:11 -0300

    Fixed bug in Register Student

--- modulo_2/exercicios-backend-api-rest/01-cadastro-de-alunos/src/controllers/studentsControllers.js
+++ modulo_2/exercicios-backend-api-rest/01-cadastro-de-alunos/src/controllers/studentsControllers.js
@@ -10,7 +10,7 @@ const getStudents = (req, res) => {
   const isValidId = (id) => !isNaN(parseFloat(id)) && isFinite(id);
   // If id is not a number, return error 400 (bad request).
   if (!isValidId(id)) {
-    return res.status(400).json({ mensage: 'Invalid Id' })
+    return res.status(400).json({ message: 'Invalid Id' })
   };
   // Search the student in database.
   const student = students.find((student) => {
@@ -18,7 +18,7 @@ const getStudents = (req, res) => {
   });
   // If student is not found, return error 404 (not found).
   if (!student) {
-    return res.status(404).json({ mensage: 'Student not found' })
+    return res.status(404).json({ message: 'Student not found' })
   };
   return res.status(200).json(student)
 };
@@ -27,15 +27,19 @@ const registerStudent = (req, res) => {
   const { name, last_name, year, course } = req.body;
 
   if (!name || !last_name || !year || !course) {
-    return res.status(400).json({ mensage: 'One of the fields is not filled in.' })
+    return res.status(400).json({ message: 'One of the fields is not filled in.' })
   };
 
+  if (!Number(year)) {
+    return res.status(400).json({ message: 'The year is Not a Number.' })
+  }
+
   if (!name.trim() || !last_name.trim() || !course.trim()) {
-    return res.status(400).json({ mensage: 'The fields (name, last name and course) must be text only.' })
+    return res.status(400).json({ message: 'The fields (name, last name and course) must be text only.' })
   }
 
   if (year < 18) {
-    return res.status(422).json({ mensage: 'Underage students are not allowed' })
+    return res.status(422).json({ message: 'Underage students are not allowed' })
   };
 
   const student = {
