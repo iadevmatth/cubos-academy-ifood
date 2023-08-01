4a6cb60d13eaaa1813e05c163ea9b52d394843a5
MatthSil <matheus.devbackend@gmail.com>
2023-07-31 21:11:38 -0300

    Resolution exercise 01 about shipping calculator with APIREST

--- modulo_2/exercicios-backend-programacao-assincrona/01-calculo-de-frete/src/controllers/shippingController.js
+++ modulo_2/exercicios-backend-programacao-assincrona/01-calculo-de-frete/src/controllers/shippingController.js
@@ -1,12 +1,13 @@
 const products = require('../dataBase/products');
-const shipping = require('../dataBase/businessRules');
+const { getStateFromZipcode } = require('utils-playground');
+const shippingPrices = require('../dataBase/businessRules');
 
 // List all products
-const listProducts = (req, res) => {
+const listProducts = async (req, res) => {
   return res.status(200).json(products)
 };
 // Search a specific product
-const searchProduct = (req, res) => {
+const searchProduct = async (req, res) => {
   const { id } = req.params;
 
   const isValidId = Number(id);
@@ -26,8 +27,43 @@ const searchProduct = (req, res) => {
   return res.status(200).json(product)
 };
 
+const calcShipping = async (req, res) => {
+  const { id, cep } = req.params;
+
+  const isValidId = Number(id);
+
+  if (!isValidId) {
+    return res.status(400).json({ message: 'Invalid ID' })
+  };
+
+  const product = products.find((product) => {
+    return product.id === Number(id)
+  })
+
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' });
+  };
+
+  const state = await getStateFromZipcode(cep);
+
+  let shippValue = 0;
+
+  if (shippingPrices.hasOwnProperty(state)) {
+    shippValue = product.valor * shippingPrices[state];
+  } else {
+    shippValue = product.valor * shippingPrices.normalCalc
+  }
+
+  return res.json({
+    product,
+    state,
+    frete: shippValue
+  });
+};
+
 
 module.exports = {
   listProducts,
-  searchProduct
+  searchProduct,
+  calcShipping
 };
--- modulo_2/exercicios-backend-programacao-assincrona/01-calculo-de-frete/src/routes.js
+++ modulo_2/exercicios-backend-programacao-assincrona/01-calculo-de-frete/src/routes.js
@@ -5,5 +5,6 @@ const routes = express();
 // Routes referring at an products
 routes.get('/products', shippingController.listProducts);
 routes.get('/products/:id', shippingController.searchProduct);
+routes.get('/products/:id/shipping/:cep', shippingController.calcShipping);
 
 module.exports = routes
