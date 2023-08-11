eeb66f9dd0bbe432f8812aeec801b639dfccd560
MatthSil <matheus.devbackend@gmail.com>
2023-08-11 02:13:01 -0300

    Resolução exercicio consulta sql

---
+++ modulo_3/exercicios-backend-consultas-sql/01-consulta-sql/query.sql
@@ -0,0 +1,18 @@
+select compositor, id from musicas;
+select composicao, tempo from musicas;
+select compositor, composicao from musicas where id between 47 and 123;
+select * from musicas where compositor is not null and tempo < 300 and compositor != 'Bach';
+select composicao, tempo from musicas where compositor = 'Mozart' or compositor = 'Bach';
+select * from musicas order by id desc;
+select * from musicas order by tempo desc;
+select * from musicas order by tempo asc limit 5;
+select * from musicas order by tempo desc limit 10;
+select * from musicas order by tempo asc limit 10 offset 5;
+select * from musicas order by id limit 10 offset 40;
+select * from musicas order by id limit 12 offset 72;
+select distinct compositor from musicas where compositor is not null;
+select distinct compositor, composicao from musicas;
+select * from musicas where compositor like 'Bra%';
+select * from musicas where ritmo like '%troppo';
+select * from musicas where composicao ilike '%quartet%';
+select * from musicas where composicao not ilike '%quintet%';
